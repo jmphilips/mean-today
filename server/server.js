2437cb47deab9f878cb52d2ab28b240023ae9a73
Joshua Philips <jmichaelphilips@gmail.com>
2016-10-03 15:23:03 -0500

    Added the first route to the index page

--- server/server.js
+++ server/server.js
@@ -1,2 +1,19 @@
 'use strict'
 
+const express = require('express')
+
+const app = express();
+
+app.use(express.static('client'));
+
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use((req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`)
+})
